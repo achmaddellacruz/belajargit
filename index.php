a1d9feb1e3c7869c0a10c847389d354504732933
Your Name <you@example.com>
2024-12-20 11:25:25 +0700

    Memperbarui index.php dan menambahkan folder assets

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Git </title>
+</head>
+<body>
+
+    <div>
+    <h1>hellow world</h1>
+    </div>
+
+    <h1>Latihan Git</h1>
+    <button>submit</button>
+
+</body>
+</html>
